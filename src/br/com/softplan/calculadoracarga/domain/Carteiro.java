3736405e69cf644d1cce120ab142daf60b40935e
Vinicius Mattei <viniciuspmattei@gmail.com>
2018-08-15 16:55:24 -0300

    Commit v1.6

---
+++ src/br/com/softplan/calculadoracarga/domain/Carteiro.java
@@ -0,0 +1,9 @@
+package br.com.softplan.calculadoracarga.domain;
+
+public class Carteiro extends Veiculo{
+	
+	public double getPrecoVeiculo() {
+		return 1.5;
+	}
+	
+}
---
+++ src/br/com/softplan/calculadoracarga/domain/JumentoCarregador.java
@@ -0,0 +1,9 @@
+package br.com.softplan.calculadoracarga.domain;
+
+public class JumentoCarregador extends Veiculo{
+	
+	public double getPrecoVeiculo() {
+		return 0.80;
+	}
+	
+}
